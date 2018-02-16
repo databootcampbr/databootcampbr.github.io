eee88a322f4a3711f4ff1bb4e59c66da412bb1ab
Renan Oliveira <renan@corp.globo.com>
2018-02-16 13:47:46 -0200

    cupom

--- cupom.js
+++ cupom.js
@@ -1,4 +1,4 @@
-if (window.location.search.substring(1) === "cupom=pyladies" ) {
+if (window.location.search.substring(1) === "cupom=pyladies" || window.location.search.substring(1) === "cupom=caio.boratto" ) {
     $(".prestacao").text("5x R$ 220,00")
     $("#hosted_button_id").val("XB8F2NGYQQAYW")
 }

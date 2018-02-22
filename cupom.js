946bc136628472d72c01f4d452fab66c92ccbfc5
Renan Oliveira <renan.gpa@gmail.com>
2018-02-22 20:15:46 -0300

    rio com desconto

--- cupom.js
+++ cupom.js
@@ -7,3 +7,8 @@
 //   $(".prestacao").text("5x R$ 220,00")
 //   $("#hosted_button_id").val("7CFCD6EN4FED8")
 // }
+
+if (window.location.search.substring(1) === "cupom=alumni") {
+  $(".prestacao").text("4x R$ 337,50")
+  $("#hosted_button_id").val("NC7EVB4M46AGG")
+}
